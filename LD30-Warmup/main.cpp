1cee45a53844f79f429e358b49393288d9fb1427
Xtreme Prime <XtremePrimeDev@gmail.com>
2014-08-13 21:10:19 +0300

    SFW

--- LD30-Warmup/main.cpp
+++ LD30-Warmup/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, Ara!" << std:: endl;
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(800,600), "Untitled Project", sf::Style::Titlebar | sf::Style::Close);
 
-    return 0;
+    while(window.isOpen())
+    {
+         sf::Event event;
+         while(window.pollEvent(event))
+         {
+             if(event.type == sf::Event::Closed)
+                 window.close();
+         }
+         window.display();
+    }
 }
