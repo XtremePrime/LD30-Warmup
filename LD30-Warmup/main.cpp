6ad89a44fadc72dc4d2062ca7697b1310b94e688
Xtreme Prime <XtremePrimeDev@gmail.com>
2014-08-16 01:07:17 +0300

    Text testing and a font. HEYO

--- LD30-Warmup/main.cpp
+++ LD30-Warmup/main.cpp
@@ -1,19 +1,55 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
 int main()
 {
     sf::RenderWindow window;
-    window.create(sf::VideoMode(800,600), "Untitled Project", sf::Style::Titlebar | sf::Style::Close);
+    window.create(sf::VideoMode(800,600), "Untitled game", sf::Style::Titlebar | sf::Style::Close);
+
+    //Text preparation
+    sf::Font font;
+    if(!font.loadFromFile("dungeon.ttf"))
+        std::cout << "Font not found..." << std::endl;
+    sf::String sentence;
+    sf::Text text(sentence, font, 40);
+    text.setColor(sf::Color(255, 0, 0));
+    text.setStyle(sf::Text::Bold);
 
     while(window.isOpen())
     {
-         sf::Event event;
-         while(window.pollEvent(event))
-         {
-             if(event.type == sf::Event::Closed)
-                 window.close();
-         }
-         window.display();
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                case sf::Event::KeyPressed:
+                    if(event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    if(event.key.code == sf::Keyboard::Return)
+                        sentence += "\n";
+
+                case sf::Event::TextEntered:
+                    if(event.text.unicode >= 32 && event.text.unicode <= 126)
+                        sentence += (char)event.text.unicode;
+                    else if(event.text.unicode == 8 && sentence.getSize() > 0)
+                        sentence.erase(sentence.getSize() - 1, sentence.getSize());
+
+                    // if(text.getLocalBounds().width >= 550)
+                    //     sentence += "\n";
+
+                    text.setString(sentence);
+
+
+                    break;
+            }
+        }
+        window.draw(text);
+        window.display();
+        window.clear();
     }
 }
