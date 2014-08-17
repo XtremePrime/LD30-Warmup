75c98847769f8cbbcea4ca4eb8ca48b1340503af
Xtreme Prime <XtremePrimeDev@gmail.com>
2014-08-18 02:46:01 +0300

    Quite some changes
    Moved the stuff in a res folder, added @aramande 's bg image to the game
    and found out why unicode was broken :3

--- LD30-Warmup/main.cpp
+++ LD30-Warmup/main.cpp
@@ -4,17 +4,29 @@
 
 int main()
 {
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
     sf::RenderWindow window;
-    window.create(sf::VideoMode(800,600), "Untitled game", sf::Style::Titlebar | sf::Style::Close);
+    window.create(sf::VideoMode(WIDTH,HEIGHT), "Untitled game", sf::Style::Titlebar | sf::Style::Close);
 
     //Text preparation
     sf::Font font;
-    if(!font.loadFromFile("dungeon.ttf"))
+    if(!font.loadFromFile("res/dungeon.ttf"))
         std::cout << "Font not found..." << std::endl;
     sf::String sentence;
     sf::Text text(sentence, font, 40);
-    text.setColor(sf::Color(255, 0, 0));
+    text.setColor(sf::Color(0, 0, 0));
     text.setStyle(sf::Text::Bold);
+
+    //Texture prep
+    sf::Texture texture;
+    if (!texture.loadFromFile("res/bg.png"))
+        std::cout << "Texture not found..." << std::endl;
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    //Stretching image to fill screen
+    sprite.setScale(WIDTH/sprite.getLocalBounds().width, HEIGHT/sprite.getLocalBounds().height);
 
     while(window.isOpen())
     {
@@ -33,21 +45,24 @@ int main()
                     if(event.key.code == sf::Keyboard::Return)
                         sentence += "\n";
 
+                    break;
                 case sf::Event::TextEntered:
-                    if(event.text.unicode >= 32 && event.text.unicode <= 126)
+                    if(event.text.unicode >= 32 && event.text.unicode <= 128){
                         sentence += (char)event.text.unicode;
-                    else if(event.text.unicode == 8 && sentence.getSize() > 0)
+                    }
+                    else if((char)event.text.unicode == '\b' && sentence.getSize() > 0){
                         sentence.erase(sentence.getSize() - 1, sentence.getSize());
-
-                    // if(text.getLocalBounds().width >= 550)
-                    //     sentence += "\n";
+                    }
 
                     text.setString(sentence);
 
+                    std::cout << text.getGlobalBounds().width << "/" << text.getGlobalBounds().height << std::endl;
 
                     break;
             }
         }
+
+        window.draw(sprite);
         window.draw(text);
         window.display();
         window.clear();
